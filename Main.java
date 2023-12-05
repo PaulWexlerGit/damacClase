0e9cd528f79565e5c873c19b7873c30752be8692
Pablo Mesa Pérez <pablomesa@outlook.es>
2023-12-05 20:51:36 +0100

    quitado el espacio

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public Main{
 	public static void main(String[] args){
-		System.out.println("patatas señorita !!");
+		System.out.println("patatas señorita!!");
 		System.out.println("patatita de verdad");
 	}
 }
